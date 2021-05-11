81d388e0e59cbfe48195648b3e8d789b2f1c405d
Alex Standiford <a@alexstandiford.com>
2021-05-11 11:35:09 -0700

    Corrects underpin autoloader

--- admin-pages.php
+++ admin-pages.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add this loader.
 add_action( 'underpin/before_setup', function ( $file, $class ) {
-	if ( ! defined( 'UNDERPIN_ADMIN_MENU_ROOT_DIR' ) ) {
+	if ( ! defined( 'UNDERPIN_ADMIN_PAGES_ROOT_DIR' ) ) {
 		define( 'UNDERPIN_ADMIN_PAGES_ROOT_DIR', plugin_dir_path( __FILE__ ) );
 	}
 
