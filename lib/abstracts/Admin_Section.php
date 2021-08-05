775d6a247f6ff8029605c9420545683f95f6bb15
Alex Standiford <a@alexstandiford.com>
2021-08-05 06:27:07 -0600

    Bumps logger loader version to 2.0

--- lib/abstracts/Admin_Section.php
+++ lib/abstracts/Admin_Section.php
@@ -286,7 +286,7 @@ abstract class Admin_Section {
 		if ( isset( $this->$key ) ) {
 			return $this->$key;
 		} else {
-			return new WP_error( 'batch_task_param_not_set', 'The batch task key ' . $key . ' could not be found.' );
+			return new WP_Error( 'batch_task_param_not_set', 'The batch task key ' . $key . ' could not be found.' );
 		}
 	}
 
